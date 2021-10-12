2f674daaa2eea3832c39e0fa7426caddb2f948ec
Karthikeyan <karthikeyankes@gmail.com>
2021-10-12 21:44:29 +0530

    Default setting

--- com.iwebautomata/src/main/java/com/iwebautomata/myexe/run/RunMe.java
+++ com.iwebautomata/src/main/java/com/iwebautomata/myexe/run/RunMe.java
@@ -1,22 +1,21 @@
 package com.iwebautomata.myexe.run;
 
 
-import org.apache.log4j.BasicConfigurator;  
-import org.apache.log4j.LogManager;  
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 public class RunMe {
-	private static final Logger logger = LogManager.getLogger(RunMe.class);
-	@SuppressWarnings("unused")
+	private static final Logger log = LogManager.getLogger(RunMe.class);
 	private static final MyExeInit myExeInit = new MyExeInit();  
 	public static void main (String a[])
 	{
 		boolean start=false;
-		BasicConfigurator.configure();  
-		logger.info("...Start the AIA...... ");  
+		//BasicConfigurator.configure();  
+		log.info("...Start the AIA...... ");  
 		try {
 			
-			//myExeInit.runInit();
-			//myExeInit.runInitlog();
+			myExeInit.runInit();
+			myExeInit.runInitlog();
 			start=true;
 		}
 		catch (Exception e) {System.out.println("Failed to Start");}
@@ -30,9 +29,9 @@ public class RunMe {
 	        } 
 	        catch (CatchHereException ex) 
 	        {  
-	            logger.error(ex.getMessage()); 
+	            log.error(ex.getMessage()); 
 	        } 
 		}
-		logger.info("...End the AIA...... "); 
+		log.info("...End the AIA...... "); 
 	}
 }
